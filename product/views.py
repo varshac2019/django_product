7de596baffdd5007255c9d5041b1d7e15d542726
varshac2019 <varsha.c@amazatic.com>
2019-05-13 16:27:55 +0530

    product details on new page.

--- product/views.py
+++ product/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'product/product_list.html',  {'products': products})
+
+def product_detail(request,pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product/product_detail.html', {'product': product})
+
+
