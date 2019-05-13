450199f3e9a2638184d57464f73e50ee957891ef
varshac2019 <varsha.c@amazatic.com>
2019-05-13 15:14:02 +0530

    displaying list of all products.

--- product/views.py
+++ product/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product/product_list.html',  {'products': products})
